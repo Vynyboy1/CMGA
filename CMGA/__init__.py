90453917c9d7728b25b5ed234991963908bde85e
Vinicius Gabriel <105754161+Vynyboy1@users.noreply.github.com>
2025-09-19 15:32:18 -0300

    Update __init__.py

--- CMGA/__init__.py
+++ CMGA/__init__.py
@@ -6,4 +6,11 @@ from .funcoes import (
     check_install_import
 )
 
+__all__ = [
+    "saudacao",
+    "Checa_Caminho",
+    "get_script_path",
+    "get_last_day_of_month",
+    "check_install_import"
+]
 
